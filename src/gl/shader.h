41ed596a83381f552bcab6b73c84ed64676376de
liuke <liuke.rd@bytedance.com>
2022-08-22 21:35:32 +0800

    添加设置shader中uniform值API

--- src/gl/shader.h
+++ src/gl/shader.h
@@ -30,12 +30,31 @@ namespace plan9 {
         static std::tuple<bool, GLuint, std::string> compile_shader(GLuint vertex_shader, GLuint fragment_shader);
 
         shader(const std::string &vertex_file_path, const std::string &fragment_file_path);
-
+        /**
+         * 编译shader文件，编译后才能使用get_id
+         * @return true表示编译成功
+         */
         bool compile();
-
+        /**
+         * 获取编译后的shader的id
+         * @return
+         */
         GLuint get_id();
+        /**
+         * 使用shader有两种方式，一种是使用get_id返回shader的id，调用glUseProgram(id)
+         * 另一种是直接调用use()
+         */
+        void use() const;
 
         std::string  get_error();
+
+        void set_uniform_value(const std::string &param_name, bool value) const;
+        void set_uniform_value(const std::string &param_name, int value) const;
+        void set_uniform_value(const std::string &param_name, float value) const;
+        void set_uniform_value(const std::string &param_name, float value1, float value2, float value3, float value4) const;
+        void set_uniform_value(const std::string &param_name, int value1, int value2, int value3, int value4) const;
+        void set_uniform_value(const std::string &param_name, float value1, float value2, float value3) const;
+        void set_uniform_value(const std::string &param_name, int value1, int value2, int value3) const;
     private:
         class shader_impl;
         std::shared_ptr<shader_impl> impl;
