6935443efbe2a560775718a26144c06879ceb2ba
liuke <m18611419854@163.com>
2022-08-24 00:38:19 +0800

    纹理使用

--- src/gl/triangle.cpp
+++ src/gl/triangle.cpp
@@ -16,9 +16,18 @@ public:
         shader->compile();
 
         float vertices[] = {
-                -0.5f, -0.5f, -0.f, // left
-                0.5f, -0.5f, 0.f, // right
-                0.0f,  0.5f, -0.f  // top
+            0.5f, 0.5f, 0.0f, 1.0f, 1.f,  // 右上角
+            0.5f, -0.5f, 0.0f, 1.f, 0.f,  // 右下角
+            -0.5f, -0.5f, 0.0f, 0.f, 0.f,// 左下角
+            -0.5f, 0.5f, 0.0f, 0.f, 1.f,   // 左上角
+        };
+
+        unsigned int indices[] = {
+            // 注意索引从0开始!
+            // 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+            // 这样可以由下标代表顶点组合成矩形
+            0, 1, 3, // 第一个三角形
+            1, 2, 3  // 第二个三角形
         };
 
         GLuint VBO;
@@ -30,21 +39,30 @@ public:
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
         glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) nullptr);
+        GLuint EBO;
+        glGenBuffers(1, &EBO);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) nullptr);
         glEnableVertexAttribArray(0);
+
+//        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*) 3);
+//        glEnableVertexAttribArray(1);
 
         // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-        // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
-        // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
-        glBindVertexArray(0);
+//        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);//线框模式
+//        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);//填充模式
     }
 
     void render() const {
         shader->use();
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAO);
+//        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
     }
 private:
     GLuint VAO;
